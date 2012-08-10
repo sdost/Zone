890e55b68288dc4117b0878a1f8829b779f2b64a
Samuel Dost <samuel.dost@gmail.com>
2012-08-10 00:23:38 -0400

    Initial Commit

---
+++ Zone/ios/AppController.h
@@ -0,0 +1,17 @@
+//
+//  ZoneAppController.h
+//  Zone
+//
+//  Created by Samuel Dost on 8/10/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@end
+
---
+++ Zone/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  ZoneAppController.h
+//  Zone
+//
+//  Created by Samuel Dost on 8/10/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
